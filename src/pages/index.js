40659703b46c38e76d2cedca005d632d91765a5e
KPorus <designerfardin2021@gmail.com>
2023-09-19 17:47:33 +0600

    add review cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 const RootLayouts = dynamic(() => import("@/components/Layouts/RootLayouts"));
 const Banner = dynamic(() => import("@/components/Banner/Banner"));
 const Product = dynamic(() => import("@/components/HomeProduct/Product"));
+const Review = dynamic(() => import("@/components/HomeReviews/Review"));
 
 const HomePage = () => {
   return (
@@ -15,6 +16,9 @@ const HomePage = () => {
         <Banner />
         <div className="mb-12">
           <Product></Product>
+        </div>
+        <div className="mb-12">
+          <Review/>
         </div>
         <h1 className='text-center text-2xl p-5'>This is my home Page</h1>
       </div>
